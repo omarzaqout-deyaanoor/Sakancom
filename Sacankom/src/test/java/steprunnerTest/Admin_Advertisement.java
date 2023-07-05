21efb88b4041a21617ae0a69933c64d1cdf5d14d
omarzaqout <omar02-2012@hotmail.com>
2023-07-05 07:35:38 -0700

    last version add feature advertisement

---
+++ Sacankom/src/test/java/steprunnerTest/Admin_Advertisement.java
@@ -0,0 +1,104 @@
+package steprunnerTest;
+
+import classes.admin_advertisement;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Admin_Advertisement {
+
+	admin_advertisement admin_ad;
+	
+	
+	
+	@Given("a user wants to advertise a housing property")
+	public void a_user_wants_to_advertise_a_housing_property() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("the user fills in the advertisement request form with the property details")
+	public void the_user_fills_in_the_advertisement_request_form_with_the_property_details() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("submits the request")
+	public void submits_the_request() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("the request should be sent to the administrator for review")
+	public void the_request_should_be_sent_to_the_administrator_for_review() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("the user should receive a confirmation message")
+	public void the_user_should_receive_a_confirmation_message() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("an advertisement request for a housing property is pending")
+	public void an_advertisement_request_for_a_housing_property_is_pending() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("the administrator reviews the request")
+	public void the_administrator_reviews_the_request() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("the request is {string}")
+	public void the_request_is(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("the advertisement for the housing property should be published")
+	public void the_advertisement_for_the_housing_property_should_be_published() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("the user who requested the advertisement should be notified of the acceptance")
+	public void the_user_who_requested_the_advertisement_should_be_notified_of_the_acceptance() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("an advertisementt requestt for a housing propertty is pendingg")
+	public void an_advertisementt_requestt_for_a_housing_propertty_is_pendingg() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("the administratorr reviewss the requestt")
+	public void the_administratorr_reviewss_the_requestt() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("the requests is {string}")
+	public void the_requests_is(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("the advertisement for the housing property should not be published")
+	public void the_advertisement_for_the_housing_property_should_not_be_published() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("the user who requested the advertisement should be notified of the rejection")
+	public void the_user_who_requested_the_advertisement_should_be_notified_of_the_rejection() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+	
+}
